b341bcb0031ddaa8d59680f0ed88bc77f04ef701
7shiroi <lucky7kelvin@gmail.com>
2022-03-10 14:30:05 +0800

    feat: delivery

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const route = require('express').Router();
 const responseHandler = require('../helpers/responseHandler');
 
+route.use('/delivery_type', require('./deliveryType'));
+
 route.get('/', (req, res) => responseHandler(res, 200, 'Backend is running well!'));
 
 module.exports = route;
