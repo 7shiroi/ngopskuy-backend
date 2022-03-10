c6864e7a2e79a6c27fa7a2a8c46fb7d1effbf1e7
jumelisah <jumelisa09@gmail.com>
2022-03-10 11:43:58 +0700

    Delete product.js

--- src/routes/product.js
+++
@@ -1,9 +0,0 @@
-const product = require('express').Router();
-const { getProduct, addProduct, editProduct } = require('../controller/product');
-
-product.get('/', getProduct);
-product.post('/', addProduct);
-// product.patch('/', editProduct);
-// product.patch('/:id', editProduct);
-
-module.exports = product;
