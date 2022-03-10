8f62ea557878eb7b39ef3c6722609d5371d96ee0
zalfanuramalia <zalfafanur@gmail.com>
2022-03-10 12:18:29 +0700

    merge package

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,10 @@ route.use('/auth', require('./auth'));
 
 route.get('/', (req, res) => responseHandler(res, 200, 'Backend is running well!'));
 
+route.use('/auth', require('./auth'));
+
+route.use('/user', require('./user'));
+
+route.use('/profil', require('./profil'));
+
 module.exports = route;
