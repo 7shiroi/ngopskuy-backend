13e7b8a813b584a2557dbfcd05fce4407204bb59
DanangFirmantara <danangfirmantara837@gmail.com>
2022-03-11 16:40:31 +0700

    feat : create update and patch

---
+++ src/models/promoDeliveryType.js
@@ -0,0 +1,59 @@
+const db = require('../helpers/db');
+
+exports.getIdPromo = (data) => new Promise((resolve, reject) => {
+  db.query('SELECT * FROM promo WHERE id = ?', [data.id_promo], (err, res) => {
+    if (err) reject(err);
+    resolve(res);
+  });
+});
+
+exports.getIdDeliveryType = (data) => new Promise((resolve, reject) => {
+  db.query('SELECT * FROM delivery_type WHERE id = ?', [data.id_delivery_type], (err, res) => {
+    if (err) reject(err);
+    resolve(res);
+  });
+});
+
+exports.getIsOnlyOne = (data) => new Promise((resolve, reject) => {
+  db.query('SELECT * FROM promo_delivery_type WHERE id_promo=? AND id_delivery_type=?', [data.id_promo, data.id_delivery_type], (err, res) => {
+    if (err) reject(err);
+    resolve(res);
+  });
+});
+
+exports.postPromoDeliveryType = (data) => new Promise((resolve, reject) => {
+  db.query('INSERT INTO promo_delivery_TYPE SET ?', [data], (err, res) => {
+    if (err) reject(err);
+    resolve(res);
+  });
+});
+
+exports.getData = (data) => new Promise((resolve, reject) => {
+  db.query(`SELECT pdt.id , p.name as product_promo_name, dt.name as delivery_type_name FROM promo_delivery_type pdt 
+  LEFT JOIN promo p ON pdt.id_promo = p.id 
+  LEFT JOIN delivery_type dt ON pdt.id_delivery_type = dt.id WHERE pdt.id_promo=${data.id_promo} AND pdt.id_delivery_type = ${data.id_delivery_type}`, (err, res) => {
+    if (err) reject(err);
+    resolve(res);
+  });
+});
+
+exports.getDataById = (data) => new Promise((resolve, reject) => {
+  db.query('SELECT * FROM promo_delivery_type WHERE id = ?', [data.id], (err, res) => {
+    if (err) reject(err);
+    resolve(res);
+  });
+});
+
+exports.patchPromoDeliveryType = (data) => new Promise((resolve, reject) => {
+  db.query('UPDATE promo_delivery_type SET id_delivery_type = ? WHERE id = ?', [data.id_delivery_type, data.id], (err, res) => {
+    if (err) reject(err);
+    resolve(res);
+  });
+});
+
+exports.deletedPromoDeliveryType = (id) => new Promise((resolve, reject) => {
+  db.query('DELETE promo_delivery_type WHERE id = ?', [id], (err, res) => {
+    if (err) reject(err);
+    resolve(res);
+  });
+});
--- src/routes/promoDeliveryType.js
+++ src/routes/promoDeliveryType.js
@@ -2,5 +2,7 @@ const promoDeliveryType = require('express').Router();
 const promoDeliveryTypeController = require('../controllers/promoDeliveryType');
 
 promoDeliveryType.post('/', promoDeliveryTypeController.postPromoDeliveryType);
+promoDeliveryType.patch('/', promoDeliveryTypeController.patchPromoDeliveryType);
+promoDeliveryType.delete('/', promoDeliveryTypeController.deletedPromoDeliveryType);
 
 module.exports = promoDeliveryType;
