de39ed310a9cb91bdbe35b7ded3460df1e186045
DanangFirmantara <danangfirmantara837@gmail.com>
2022-03-14 20:21:34 +0700

    feat : fix error

--- src/routes/promoDeliveryType.js
+++ src/routes/promoDeliveryType.js
@@ -3,6 +3,7 @@ const promoDeliveryTypeController = require('../controllers/promoDeliveryType');
 const { verifyUser } = require('../helpers/auth');
 
 promoDeliveryType.get('/', promoDeliveryTypeController.getPromoDeliveryType);
+promoDeliveryType.get('/:id_promo',promoDeliveryTypeController.getPromoDeliveryTypeByIdPromo)
 promoDeliveryType.post('/', verifyUser, promoDeliveryTypeController.postPromoDeliveryType);
 promoDeliveryType.patch('/', verifyUser, promoDeliveryTypeController.patchPromoDeliveryType);
 promoDeliveryType.delete('/', verifyUser, promoDeliveryTypeController.deletedPromoDeliveryType);
