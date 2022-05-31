150a557dc98949c8b65118b4cac4e69040931c20
7shiroi <lucky7kelvin@gmail.com>
2022-05-31 11:35:25 +0800

    refactor: remove commented code

--- src/helpers/upload.js
+++ src/helpers/upload.js
@@ -33,17 +33,6 @@ const storage = new CloudinaryStorage({
   },
 });
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'uploads/');
-//   },
-//   filename: (req, file, cb) => {
-//     const fName = file.originalname.split('.');
-//     const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
-//     cb(null, `${fName[0]}-${uniqueSuffix}.${fName[fName.length - 1]}`);
-//   },
-// });
-
 function imageFileFilter(req, file, cb) {
   const supportedMimeType = ['image/jpeg', 'image/png', 'image/gif', 'image/svg+xml', 'image/tiff'];
   if (!supportedMimeType.includes(file.mimetype)) {
