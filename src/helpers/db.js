de918b1a03f70b4c5a9ad208ebf04fb830579984
7shiroi <lucky7kelvin@gmail.com>
2022-05-31 10:43:52 +0800

    fix: change to create pool for deployment

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -4,13 +4,11 @@ const {
   DB_HOST, DB_USER, DB_PASSWORD, DB_NAME,
 } = process.env;
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
 });
-
-db.connect();
 
 module.exports = db;
